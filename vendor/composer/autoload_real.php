df7036e60fa36ff644660b1b580a7c134d885834
Gilbert <arioadiyoso@gmail.com>
2026-03-15 12:24:52 +0700

    add

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,18 +33,25 @@ class ComposerAutoloaderInit84d0f78483506de343557b38c47b1d59
 
         $loader->register(true);
 
-        $filesToLoad = \Composer\Autoload\ComposerStaticInit84d0f78483506de343557b38c47b1d59::$files;
-        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-                require $file;
-            }
-        }, null, null);
-        foreach ($filesToLoad as $fileIdentifier => $file) {
-            $requireFile($fileIdentifier, $file);
+        $includeFiles = \Composer\Autoload\ComposerStaticInit84d0f78483506de343557b38c47b1d59::$files;
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire84d0f78483506de343557b38c47b1d59($fileIdentifier, $file);
         }
 
         return $loader;
+    }
+}
+
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
+function composerRequire84d0f78483506de343557b38c47b1d59($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ce13db0c059ff45cf0671af571b32236ae516a4d',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -398,9 +398,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ce13db0c059ff45cf0671af571b32236ae516a4d',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
